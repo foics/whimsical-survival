9176812966e8b4cf0965e49031db7bcafd42dcf3
foics <76219388+foics@users.noreply.github.com>
2025-04-19 11:40:52 -0400

    minor collision bug fix

--- src/foxcore/collision.cpp
+++ src/foxcore/collision.cpp
@@ -1,7 +1,7 @@
 #include "foxcore/collision.hpp"
 
 bool isOverlapping(Object *object, Object *otherObject) {
-    if (object->x + object->width >= otherObject->x && object->x <= otherObject->x + otherObject->width && object->y + object->height >= otherObject->y && object->y <= otherObject->y + otherObject->width) {
+    if (object->x + object->width >= otherObject->x && object->x <= otherObject->x + otherObject->width && object->y + object->height >= otherObject->y && object->y <= otherObject->y + otherObject->height) {
         return true;
     } else {
         return false;
